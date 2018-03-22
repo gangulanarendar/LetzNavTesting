da909cab258aeb7f0d7c48d2bc5802903f98acdb
Narendar Gangula <gangulanarendar@gmail.com>
2018-03-22 15:38:01 +0530

    mar 22 23

--- src/main/java/letzNavApplicationsToTest/clarityPPM.java
+++ src/main/java/letzNavApplicationsToTest/clarityPPM.java
@@ -47,8 +47,8 @@ public class clarityPPM {
 	public void goToPortlets() {
 	
 	 
-		driver.navigate().to("https://en.wikipedia.org/wiki/Braxton_Bragg#Historical_reputation");
-	//	driver.get(arg0);
+		//driver.navigate().to("https://en.wikipedia.org/wiki/Braxton_Bragg#Historical_reputation");
+		driver.get("http://111.93.27.187:8889/niku/nu#action:npt.myPortlets");
 		File src= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		try {
 			
