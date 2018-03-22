dd036d6ecfc51eca13072266a696c573ba83c8ea
Narendar Gangula <gangulanarendar@gmail.com>
2018-03-22 15:03:18 +0530

    mar 22 18

--- src/main/java/letzNavTestFrameWork/letzNavConfiguration/letzNavInstaller.java
+++ src/main/java/letzNavTestFrameWork/letzNavConfiguration/letzNavInstaller.java
@@ -24,7 +24,7 @@ public class letzNavInstaller extends letzNavPropertiesReader {
 		try {
 			FileUtils.cleanDirectory(extensionResources);
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
+			
 			//e1.printStackTrace();
 		}
 		if (extensionName.equalsIgnoreCase("player")) {
